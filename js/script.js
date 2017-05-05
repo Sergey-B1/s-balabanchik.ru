a597250851c0ad439d14c2414b66544c5113e889
Sergey <balabansergey-@mail.ru>
2017-05-05 23:08:25 +0400

    5 mai 2015

--- js/script.js
+++ js/script.js
@@ -12,3 +12,23 @@
     })
     $("#lightgallery").lightGallery();
 });
+// Слайдер "Artists"
+    $('.img3-block1__artists--right-block1').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        infinite: true,
+        asNavFor: '.img3-block1__artists--bottom'
+    });
+    $('.img3-block1__artists--bottom').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.img3-block1__artists--right-block1',
+        dots: false,
+        centerMode: false,
+        focusOnSelect: true,
+        infinite: true,
+        prevArrow: "<div class='artists__slider-prev'><i class='fa fa-chevron-circle-left' aria-hidden='true'></i></div>",
+        nextArrow: "<div class='artists__slider-next'><i class='fa fa-chevron-circle-right' aria-hidden='true'></i></div>"
+    });
